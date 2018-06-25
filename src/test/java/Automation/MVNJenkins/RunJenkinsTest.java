bdb36540ed8d964b9ae9824b42cc351125b36215
Padmavathi <padma.sd@gmail.com>
2018-06-25 10:21:52 -0700

    Adding MVNJenkins to GitHub

---
+++ src/test/java/Automation/MVNJenkins/RunJenkinsTest.java
@@ -0,0 +1,12 @@
+package Automation.MVNJenkins;
+
+import org.testng.annotations.Test;
+
+public class RunJenkinsTest {
+	
+	@Test
+	public void printJenkins() {
+		System.out.println("In jenkins");
+	}
+
+}
---
+++ src/test/java/Automation/MVNJenkins/RunMavenTest.java
@@ -0,0 +1,11 @@
+package Automation.MVNJenkins;
+
+import org.testng.annotations.Test;
+
+public class RunMavenTest {
+
+	@Test
+	public void printMaven() {
+		System.out.println("In Maven");
+	}
+}
