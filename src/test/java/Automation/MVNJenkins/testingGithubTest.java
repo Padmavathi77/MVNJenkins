089a910af03a2161db8655cf295eebfe21901a0d
Padmavathi <padma.sd@gmail.com>
2018-06-25 10:26:42 -0700

    Adding changes to GitHub

---
+++ src/test/java/Automation/MVNJenkins/testingGithubTest.java
@@ -0,0 +1,12 @@
+package Automation.MVNJenkins;
+
+import org.testng.annotations.Test;
+
+public class testingGithubTest {
+	
+	@Test
+	public void PushToGitHub() {
+		System.out.println("First push to GitHub");
+	}
+
+}
